83b3896d8ecc6a1dc235a326fbe541af0f8f3ab8
Mercer192 <36523174+Mercer192@users.noreply.github.com>
2020-01-30 15:21:41 -0500

    Add files via upload

---
+++ Lab02/driver.ino
@@ -0,0 +1,39 @@
+#include "Arduino.h"
+#include "Motor.h"
+#include "Robot.h"
+
+
+
+#define SPEED 140
+#define TURN_SPEED 160
+
+#define frontRightdirpin1  22
+#define frontRightdirpin2  24
+#define frontRightspdpin 9
+
+#define frontLeftdirpin1  26
+#define frontLeftdirpin2  28
+#define frontLeftspdpin  10
+
+#define rearRightdirpin1  5
+#define rearRightdirpin2  6
+#define rearRightspdpin  11
+
+#define rearLeftdirpin1  7
+#define rearLeftdirpin2  8
+#define rearLeftspdpin  12
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Robot car( frontLeftdirpin1, frontLeftdirpin2, frontLeftspdpin, frontRightdirpin1, frontRightdirpin2, frontRightspdpin,
+            rearLeftdirpin1, rearLeftdirpin2, rearLeftspdpin, rearRightdirpin1, rearRightdirpin2, rearRightspdpin);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
